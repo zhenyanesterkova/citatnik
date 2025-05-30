07008654e4b391e4d7be1f4ff9d5f777efd4f13b
EvgeniyaNesterkova <miledieva08@gmail.com>
2025-05-30 19:44:41 +0300

    feat: add get random handler + fix getAll to get - get all quotes or get quotes by author - one handler

--- internal/handler/get.go
+++ internal/handler/get.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+
+	"github.com/zhenyanesterkova/citatnik/internal/app/quote"
 )
 
 const (
@@ -22,12 +24,31 @@ func (rh *RepositorieHandler) Ping(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 }
 
-func (rh *RepositorieHandler) GetAll(w http.ResponseWriter, r *http.Request) {
-	quotes := rh.repo.GetAll()
+func (rh *RepositorieHandler) Get(w http.ResponseWriter, r *http.Request) {
+	q := r.URL.Query()
+	author := q.Get("author")
+
+	var quotes []*quote.Quote
+	if author == "" {
+		quotes = rh.repo.GetAll()
+	} else {
+		quotes = rh.repo.GetByAuthor(author)
+	}
 
 	enc := json.NewEncoder(w)
 	if err := enc.Encode(quotes); err != nil {
 		log.Printf("failed encode quotes: %v", err)
+		http.Error(w, TextServerError, http.StatusInternalServerError)
+		return
+	}
+}
+
+func (rh *RepositorieHandler) GetRandom(w http.ResponseWriter, r *http.Request) {
+	quote := rh.repo.GetRandom()
+
+	enc := json.NewEncoder(w)
+	if err := enc.Encode(quote); err != nil {
+		log.Printf("failed encode random quote: %v", err)
 		http.Error(w, TextServerError, http.StatusInternalServerError)
 		return
 	}
--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -30,5 +30,6 @@ func NewRepositorieHandler(
 
 func (rh *RepositorieHandler) InitRouter(router *mux.Router) {
 	router.HandleFunc("/ping", rh.Ping).Methods(http.MethodGet)
-	router.HandleFunc("/quotes", rh.GetAll).Methods(http.MethodGet)
+	router.HandleFunc("/quotes", rh.Get).Methods(http.MethodGet)
+	router.HandleFunc("/quotes/random", rh.GetRandom).Methods(http.MethodGet)
 }

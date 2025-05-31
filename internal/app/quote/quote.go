d63dd1665028f1ed323a03beecc579e0e8dff1c3
EvgeniyaNesterkova <miledieva08@gmail.com>
2025-05-31 04:05:18 +0300

    fix: fix fieldalignment

--- internal/app/quote/quote.go
+++ internal/app/quote/quote.go
@@ -3,9 +3,9 @@ package quote
 import "time"
 
 type Quote struct {
-	ID        uint64    `json:"id"`
-	Author    string    `json:"author"`
-	Text      string    `json:"quote"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
+	Author    string    `json:"author"`
+	Text      string    `json:"quote"`
+	ID        uint64    `json:"id"`
 }
